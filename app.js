1b1c3ceca20d80601de4719a857b3e38d7061f82
kangjuyeon1 <juyeon4809@naver.com>
2023-12-02 22:35:01 +0900

    info_api

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let path = require('path'); // 프로젝트 내부에서, 파일들의 상대경
 let cookieParser = require('cookie-parser'); // 쿠키값을 다루기 위해 필요한 모듈
 let logger = require('morgan'); // 로그를 보기 쉽게 찍기 위한 모듈
 const bodyParser = require('body-parser'); // body-parser 모듈을 사용하기 위한 모듈
-
+const cors = require('cors'); // cors 모듈을 사용하기 위한 모듈
 
 // 라우팅을 해줄 경로 설정
 let indexRouter = require('./routes/index');
@@ -18,6 +18,7 @@ let app = express();
 app.use(express.static('publuc'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(cors());
 
 // 뷰 엔진으로 뭘 쓸지 정함. 우리는 ejs 사용.
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -40,10 +40,7 @@ router.get('/ExpertFilter', function(req, res, next) {
 
 //전문가 리스트 페이지 렌더링
 router.get('/ExpertList', function(req, res, next) {
-    const {city, field, minPrice, maxPrice} = req.body;
-    res.render('ExpertList'),{city, field, minPrice, maxPrice};
-
-    
+    res.render('ExpertList');
 });
 
 
